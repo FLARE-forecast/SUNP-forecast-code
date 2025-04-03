536cf3297735d5f41459936ffec4803a7dd263d3
Austin Delany <addelany@gmail.com>
2025-04-03 13:34:45 -0500

    Update combined_workflow.R

--- workflows/glm_flare_v3/combined_workflow.R
+++ workflows/glm_flare_v3/combined_workflow.R
@@ -28,10 +28,11 @@ print('done with sourcing')
 #' Generate the `config_obs` object and create directories if necessary
 
 config_obs <- yaml::read_yaml(file.path(lake_directory,'configuration',config_set_name,'observation_processing.yml'))
+print('done with configuration 1')
 configure_run_file <- "configure_run.yml"
+print('done with configuration 2')
 config <- FLAREr:::set_up_simulation(configure_run_file,lake_directory, config_set_name = config_set_name)
-
-print('done with configuration')
+print('done with configuration 3')
 #' Clone or pull from data repositories
 
 FLAREr:::get_git_repo(lake_directory,
