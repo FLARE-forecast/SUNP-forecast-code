becb4c912a7141d651661617e87b018b158e370a
Austin Delany <addelany@gmail.com>
2026-06-15 11:19:06 -0500

    Comment out FLAREr package installation
    Comment out the installation of the FLAREr package.

--- workflows/glm_flare_v3/combined_workflow.R
+++ workflows/glm_flare_v3/combined_workflow.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(lubridate)
 
 #remotes::install_github('flare-forecast/FLAREr@single-parameter')
-remotes::install_github('flare-forecast/FLAREr')
+#remotes::install_github('flare-forecast/FLAREr')
 remotes::install_github("rqthomas/GLM3r")
 Sys.setenv('GLM_PATH'='GLM3r')
 
